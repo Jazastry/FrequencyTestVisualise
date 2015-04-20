38dd798ad651a2dc682ccefa80bbe22fe88de5cf
Jazastry <yogib@mail.bg>
2015-04-20 18:56:53 +0300

    Initial with page.

---
+++ js/main.js
@@ -0,0 +1,10 @@
+'use strict';
+$('.center').click(function(){
+	$(this).removeClass('inactive');
+	$('.freqBlock.active').removeClass('active');
+});
+$('.freqBlock').click(function(){
+	$('.freqBlock.active').removeClass('active');
+	$(this).addClass('active');
+	$('.center').addClass('inactive');
+});
