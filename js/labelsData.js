43f862d6b4ddb7458e13788f3d62a0737f7e1cc2
Jazastry <yogib@mail.bg>
2015-04-21 17:24:28 +0300

    Added responsive and languages support.

---
+++ js/labelsData.js
@@ -0,0 +1,58 @@
+$(document).ready(function() {
+	var dataList = (function(){
+		var labelsFile = "../GitPageFrequencyTest/data/labelsData.json";
+
+		function loadLangData(success, lang) {
+			$.getJSON( labelsFile, {
+				format: "json"
+				}).done(function( lablesData ) {
+					success(lablesData, lang);
+			});
+		}
+
+		function loadLangList(lablesData){
+			_.each(lablesData, function(lang){
+	    		$('#countryNames').append('<li class="langList">'+lang.name+'</li>');
+	    	});
+		}
+
+		function loadPageData(pageData, lang) {
+			var langData = _.where(pageData, {name: lang})[0];
+			console.log(langData);
+			_.each(langData.params, function(param){
+				_.each(param.params, function(inerParam){
+					$('#'+param.name+'>div>.'+inerParam.name).text(inerParam.value);
+				});
+			});
+		}	
+
+		function loadLangLabels() {
+			$.getJSON( labelsFile, {
+				format: "json"
+				}).done(function( lablesData ) {
+					_.each(lablesData, function(lang){
+			    		$('#countryNames').append('<li class="langList">'+lang.name+'</li>');
+			    	});
+		    		$('.langList').on('click', function(e){
+						var lang = $(this).text();
+						dataList.loadPageLanguage(lang);
+					});
+			});
+		}
+
+		function loadPageLanguage(lang){
+			loadLangData(loadPageData, lang);
+		}
+
+		return {
+			loadLanguages: loadLangLabels,
+			loadPageLanguage: loadPageLanguage
+		};
+	}());
+
+	dataList.loadLanguages();
+
+	$("#languages").on('click', function() {
+		$('#countryNames').toggle(400);
+	});
+});
--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 'use strict';
-$('.center').click(function(){
-	$(this).removeClass('inactive');
-	$('.freqBlock.active').removeClass('active');
-});
-$('.freqBlock').click(function(){
-	$('.freqBlock.active').removeClass('active');
-	$(this).addClass('active');
-	$('.center').addClass('inactive');
+$(document).ready(function() {
+	$('.center').click(function(){
+		$(this).removeClass('inactive');
+		$('.freqBlock.active').removeClass('active');
+	});
+	$('.freqBlock').click(function(){
+		$('.freqBlock.active').removeClass('active');
+		$(this).addClass('active');
+		$('.center').addClass('inactive');
+	});
 });
